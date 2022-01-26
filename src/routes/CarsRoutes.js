edcbaa38b67f27d1eb6ca8c544a5a24d812a1e75
Nana <tsampaio21@gmail.com>
2022-01-26 18:11:44 -0300

    build: create validation for post create route

--- src/routes/CarsRoutes.js
+++ src/routes/CarsRoutes.js
@@ -1,7 +1,8 @@
 const CarController = require('../app/controllers/CarController');
+const createValidation = require('../app/validations/Cars/create');
 
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
-  routes.post('/', CarController.create);
+  routes.post('/', createValidation, CarController.create);
   server.use(prefix, routes);
 }
